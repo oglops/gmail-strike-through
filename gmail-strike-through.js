cff3e6e4d536b5a213672d79a7cb4298ca16d04f
oglop <oglops@gmail.com>
2016-02-24 22:12:44 -0800

    fix icon display on hidpi screen, set size to 21x21

--- gmail-strike-through.js
+++ gmail-strike-through.js
@@ -15,11 +15,12 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 // ==UserScript==
-// @name           Gmail Strikethrough Button
+// @name           Gmail Strikethrough+insertImg Button
 // @namespace      http://userscripts.org/scripts/show/57725
-// @description    Adds strikethrough button to Gmail rich text editor
+// @description    Adds strikethrough button and insert image button to Gmail rich text editor
 // @include        http://mail.google.com/*
 // @include        https://mail.google.com/*
+// @grant          none
 // ==/UserScript==
 var gmailStrikeThroughButton = {
     strikethroughButton: null,
@@ -28,6 +29,7 @@ var gmailStrikeThroughButton = {
     canvas_frame_document: null,
     text_editor: null,
     compose_method: '',
+    button_exist: false,
     getIcon: function() {
         var icon = "iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAAsTAAALEwEAmpwYAAAE" + "JmlUWHRYTUw6Y29tLmFkb2JlLnhtcAAAAAAAPHg6eG1wbWV0YSB4bWxuczp4PSJhZG9iZTpuczpt" + "ZXRhLyIgeDp4bXB0az0iWE1QIENvcmUgNS4xLjIiPgogICA8cmRmOlJERiB4bWxuczpyZGY9Imh0" + "dHA6Ly93d3cudzMub3JnLzE5OTkvMDIvMjItcmRmLXN5bnRheC1ucyMiPgogICAgICA8cmRmOkRl" + "c2NyaXB0aW9uIHJkZjphYm91dD0iIgogICAgICAgICAgICB4bWxuczp4bXA9Imh0dHA6Ly9ucy5h" + "ZG9iZS5jb20veGFwLzEuMC8iPgogICAgICAgICA8eG1wOk1vZGlmeURhdGU+MjAxMi0wOS0wMlQx" + "MzowOTo4NDwveG1wOk1vZGlmeURhdGU+CiAgICAgICAgIDx4bXA6Q3JlYXRvclRvb2w+UGl4ZWxt" + "YXRvciAyLjE8L3htcDpDcmVhdG9yVG9vbD4KICAgICAgPC9yZGY6RGVzY3JpcHRpb24+CiAgICAg" + "IDxyZGY6RGVzY3JpcHRpb24gcmRmOmFib3V0PSIiCiAgICAgICAgICAgIHhtbG5zOnRpZmY9Imh0" + "dHA6Ly9ucy5hZG9iZS5jb20vdGlmZi8xLjAvIj4KICAgICAgICAgPHRpZmY6T3JpZW50YXRpb24+" + "MTwvdGlmZjpPcmllbnRhdGlvbj4KICAgICAgICAgPHRpZmY6WVJlc29sdXRpb24+NzI8L3RpZmY6" + "WVJlc29sdXRpb24+CiAgICAgICAgIDx0aWZmOkNvbXByZXNzaW9uPjU8L3RpZmY6Q29tcHJlc3Np" + "b24+CiAgICAgICAgIDx0aWZmOlJlc29sdXRpb25Vbml0PjE8L3RpZmY6UmVzb2x1dGlvblVuaXQ+" + "CiAgICAgICAgIDx0aWZmOlhSZXNvbHV0aW9uPjcyPC90aWZmOlhSZXNvbHV0aW9uPgogICAgICA8" + "L3JkZjpEZXNjcmlwdGlvbj4KICAgICAgPHJkZjpEZXNjcmlwdGlvbiByZGY6YWJvdXQ9IiIKICAg" + "ICAgICAgICAgeG1sbnM6ZXhpZj0iaHR0cDovL25zLmFkb2JlLmNvbS9leGlmLzEuMC8iPgogICAg" + "ICAgICA8ZXhpZjpQaXhlbFhEaW1lbnNpb24+MTY8L2V4aWY6UGl4ZWxYRGltZW5zaW9uPgogICAg" + "ICAgICA8ZXhpZjpDb2xvclNwYWNlPjY1NTM1PC9leGlmOkNvbG9yU3BhY2U+CiAgICAgICAgIDxl" + "eGlmOlBpeGVsWURpbWVuc2lvbj4xNjwvZXhpZjpQaXhlbFlEaW1lbnNpb24+CiAgICAgIDwvcmRm" + "OkRlc2NyaXB0aW9uPgogICA8L3JkZjpSREY+CjwveDp4bXBtZXRhPgoWmbtZAAAA0klEQVQ4EcVS" + "2w3CMAwkqAt0BVZghXYVJgCxQhiBrsAIWaGM0BVghHBX7MhtU0DKB5ZOfp3PaRoXY9yU2LZkmLP/" + "F6hyn+Ccq1H3wB1gvCNwXy381HiJc4DxAM5aZwwgzXDnRRAPJAPe9pAHm2uc296IAEWuwJ5kWKND" + "1i8EhBwwwClF/1EAzZMh69A3fxyX2ePI9trWIMw7GWRBb3sjf1F4D6Q/oH0RGDRXv/YSvbwFzCV7" + "IupSJkH2IaFHcoDITWI+pg5bL/ATczxKia19ws+aL+i4Az4HM0r5AAAAAElFTkSuQmCC";
         return icon;
@@ -40,6 +42,10 @@ var gmailStrikeThroughButton = {
         gmailStrikeThroughButton.checkForStrikes();
     },
     addButton: function() {
+        if (this.underlineButton.parentNode.getElementById("strikethroughbutton")) {
+            alert("exist! return");
+            return;
+        }
         this.strikethroughButton = this.underlineButton.cloneNode(true);
         this.strikethroughButton.setAttribute("title", "Strikethrough");
         this.strikethroughButton.setAttribute("data-tooltip", "Strikethrough");
@@ -71,6 +77,7 @@ var gmailStrikeThroughButton = {
         var fix_width = false;
         var is_firefox = (navigator.userAgent.indexOf('Firefox') != -1);
         var is_chrome = (navigator.userAgent.indexOf('Chrome') != -1);
+        // if (this.button_exist) return;
         if (e != null) {
             // old compose
             if (e.target.nodeName == 'IFRAME' && e.target.className.indexOf('editable') != -1) {
@@ -104,6 +111,7 @@ var gmailStrikeThroughButton = {
         if (composing) {
             var xPathResult = gmailStrikeThroughButton.canvas_frame_document.evaluate("//div[@command='+underline']", gmailStrikeThroughButton.canvas_frame_document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
             if (gmailStrikeThroughButton.underlineButton = xPathResult.snapshotItem(0)) { // =!
+                this.button_exist = true;
                 gmailStrikeThroughButton.getActiveButtonClassName();
                 gmailStrikeThroughButton.addButton();
                 setTimeout(gmailStrikeThroughButton.bindTextEditorEvents, 1);
@@ -114,7 +122,11 @@ var gmailStrikeThroughButton = {
         if (document.body.innerHTML.indexOf("+underline") != -1) {
             gmailStrikeThroughButton.detectComponents(null);
         }
-        window.addEventListener("DOMNodeInserted", gmailStrikeThroughButton.detectComponents, false);
+
+        window.addEventListener("click", function() {
+            gmailStrikeThroughButton.detectComponents();
+        }, false);
+
     }
 }
 gmailStrikeThroughButton.init();
